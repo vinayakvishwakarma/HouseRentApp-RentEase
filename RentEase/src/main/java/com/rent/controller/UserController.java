2a065ab3c4bce1b2581c1a419727f7e4265c1e80
vinayak vishwakarma <vinayakvishwakarma1@gmail.com>
2024-08-13 23:31:43 +0530

    six

--- RentEase/src/main/java/com/rent/controller/UserController.java
+++ RentEase/src/main/java/com/rent/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.rent.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins="http://localhost:3000")
 public class UserController {
 
 	@Autowired
