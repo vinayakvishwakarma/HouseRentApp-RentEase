6f3c5dd2902cae04d71e8986b1d26b0db851a24a
vinayak vishwakarma <vinayakvishwakarma1@gmail.com>
2024-08-15 19:55:31 +0530

    nine

--- HouseRentApp-RentEase-Backend/src/main/java/com/rent/controller/PaymentController.java
+++ HouseRentApp-RentEase-Backend/src/main/java/com/rent/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.rent.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.rent.service.PaymentService;
 
 @RestController
 @RequestMapping("/payments")
+@CrossOrigin(origins="http://localhost:3000")
 public class PaymentController {
 	
 	@Autowired
--- HouseRentApp-RentEase-Backend/src/main/java/com/rent/controller/PropertyController.java
+++ HouseRentApp-RentEase-Backend/src/main/java/com/rent/controller/PropertyController.java
@@ -3,6 +3,7 @@ package com.rent.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.rent.service.PropertyService;
 
 @RestController
 @RequestMapping("/property")
+@CrossOrigin(origins="http://localhost:3000")
 public class PropertyController {
 
 	@Autowired
